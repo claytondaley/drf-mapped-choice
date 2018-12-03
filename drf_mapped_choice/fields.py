4c4bdd709ab9b4433edb9b664dcf0e5a91fb1ce0
Clayton Daley <clayton.daley@gmail.com>
2018-12-03 13:31:29 -0600

    initial commit

---
+++ drf_mapped_choice/fields.py
@@ -0,0 +1,16 @@
+from rest_framework import fields
+
+
+class MappedChoiceField(fields.ChoiceField):
+
+    @serializers.ChoiceField.choices.setter
+    def choices(self, choices):
+        self.grouped_choices = fields.to_choices_dict(choices)
+        self._choices = fields.flatten_choices_dict(self.grouped_choices)
+        self.choice_strings_to_values = {v: k for k, v in self._choices.items()}
+
+    def validate_empty_values(self, data):
+        if data == '':
+            if self.allow_blank:
+                return (True, None)
+        return super().validate_empty_values(data)
